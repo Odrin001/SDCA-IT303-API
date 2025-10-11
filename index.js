61b8b6170d7f4b7f5839f3faa3d9e6053e44d792
Audrin Paltado <audrinalan22@gmail.com>
2025-10-11 18:45:54 +0800

    API Enable course

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import 'dotenv/config'
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
 
 import { userRoutes } from './routes/user.js'
 
 const app = express()
 
+app.use(cors())
+app.use(express.json())
 app.use(express.json())
 app.use('/api/users', userRoutes)
 
